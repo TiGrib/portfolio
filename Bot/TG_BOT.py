7c7ccb84035fe5e8000077e52309dde7ad4835ce
TiGrib <yakimenkov.ka15@physics.msu.ru>
2018-12-18 16:22:41 +0300

    bot update

--- Bot/TG_BOT.py
+++ Bot/TG_BOT.py
@@ -1,25 +1,49 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
+# In[2]:
 
 
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import consts
 import logging
+import os
 
-updater=Updater(token=f'{consts.token}')
-dispatcher = updater.dispatcher
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
+
+
+
 
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+    bot.effective_message.reply_text("Hi!")
+def echo(bot, update):
+    update.effective_message.reply_text(update.effective_message.text)
+def error(bot, update, error):
+    logger.warning(f'Update {update} caused error {error}')
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+if __name__== '__main__':
+    TOKEN=consts.token
+    NAME="Itgmessage"
+    PORT=os.environ.get('PORT')
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    updater=Updater(TOKEN)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(MessageHandler(Filters.text, echo))
+    dispatcher.add_error_handler(error)
+    
+    updater.start_webhook(listen="0.0.0.0",
+                          port=PORT,
+                          url_path=TOKEN)
+    updater.bot.setWebhook(f"https://{NAME}.herokuapp.com/{TOKEN}")
+    updater.idle()
 
-updater.start_polling()
+
+
+# In[ ]:
+
+
+
 
